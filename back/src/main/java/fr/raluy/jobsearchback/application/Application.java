c32d2730c1c80e1e354f7db23ebdde1e1fa45e7f
David Raluy <david@raluy.fr>
2018-03-03 21:06:41 +0100

    Application resume is saved in database

--- back/src/main/java/fr/raluy/jobsearchback/application/Application.java
+++ back/src/main/java/fr/raluy/jobsearchback/application/Application.java
@@ -1,15 +1,20 @@
 package fr.raluy.jobsearchback.application;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import fr.raluy.jobsearchback.auth.User;
 import fr.raluy.jobsearchback.company.Company;
 import org.hibernate.annotations.Type;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.sql.Blob;
 import java.time.LocalDate;
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
+
+import static javax.persistence.FetchType.LAZY;
 
 @Entity
 @Table(name = "application")
@@ -41,7 +46,16 @@ public class Application {
     @JsonProperty ("status")
     private ApplicationStatus status;
 
+    @Column(name = "resume", length=100000)
+    @Lob
+    @JsonIgnore
+    private byte[] resume;
+
     public void setUser(User user) {
         this.user = user;
+    }
+
+    public void setResume(byte[] resume) {
+        this.resume = resume;
     }
 }
